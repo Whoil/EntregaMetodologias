e7f7e0eb8a8504259922fc022dc5eb2febcb49ac
Hector Montero <h.montero@edu.uah.es>
2026-03-17 18:46:48 +0100

    PersonTest

--- Test/Java/es/uah/matcomp/mp/e3/ejerciciosclase/PersonTest.java
+++ Test/Java/es/uah/matcomp/mp/e3/ejerciciosclase/PersonTest.java
@@ -3,8 +3,6 @@ package es.uah.matcomp.mp.e3.ejerciciosclase;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-import e3.ejerciciosclases.Person; // importamos la clase porque java no la encontraba
-
 
 class PersonTest {
 
